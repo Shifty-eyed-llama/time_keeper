93d47bd7ca1aff0875dec8df5930eea90295b318
Bryan <emtbirch@gmail.com>
2020-07-28 11:48:24 -0700

    testing time model

--- main_app/models.py
+++ main_app/models.py
@@ -18,8 +18,8 @@ class ProjectManaget(models.Manager):
 
 class Project(models.Model):
     title = models.CharField(max_length=255)
-    end_date = models.DateField()
     start_date = models.DateField()
+    end_date = models.DateField()
 
     created_by = models.ForeignKey(User, related_name = 'made_by', on_delete=models.CASCADE)
     working = models.ManyToManyField(User, related_name = "working_on")
